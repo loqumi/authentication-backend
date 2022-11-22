d68a0a6cf88b3289d3bf102f6c078635cfaa1766
loqumi <loqumihh@gmail.com>
2022-11-23 00:20:06 +0500

    ondb

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,20 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("auth_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  "freedb_auth_db",
+  "freedb_auth_db_user",
+  "p899H4K9Fa&XWYT",
+  {
+    host: "sql.freedb.tech",
+    dialect: "mysql",
+  }
+);
 
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
 export default db;
