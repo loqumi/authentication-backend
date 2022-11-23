e414a6d62c08636252b68c937cb7ab3ec10e9010
Egor Kirdyapin <74021542+loqumi@users.noreply.github.com>
2022-11-23 15:43:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -39,14 +39,8 @@ app.use(
 //   })
 // );
 
-var allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-};
+app.use(cors());
 
-app.use(allowCrossDomain);
 app.use(express.json());
 
 app.use(UserRoute);
