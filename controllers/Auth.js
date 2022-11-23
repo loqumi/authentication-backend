59581020c01ecf507d9940a1f53f17d5aa0b57b5
Egor Kirdyapin <74021542+loqumi@users.noreply.github.com>
2022-11-23 19:40:29 +0500

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -8,7 +8,7 @@ export const Login = async (req, res) => {
     },
   });
   if (user?.status) return res.status(406).json({ msg: "You are banned" });
-  console.log(user.status);
+  console.log(user?.status);
   if (!user) return res.status(404).json({ msg: "user not found" });
   const match = await argon2.verify(user.password, req.body.password);
   if (!match) return res.status(400).json({ msg: "wrong password" });
