09e0d4c91ab6129f324cdc45a1d3d94cfa7511ca
Egor Kirdyapin <74021542+loqumi@users.noreply.github.com>
2022-11-23 15:30:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -39,36 +39,19 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://loqumi-auth-app.web.app"
-  );
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
-});
+};
 
-app.use(express.json());
-app.use(UserRoute);
-app.use(AuthRoute);
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.json());
+  app.use(UserRoute);
+  app.use(AuthRoute);
+});
 
 store.sync();
 
