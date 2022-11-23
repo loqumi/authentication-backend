f5218e93e632eccefcdbfd7ece0a57de6de92b01
Egor Kirdyapin <74021542+loqumi@users.noreply.github.com>
2022-11-23 15:55:07 +0500

    Update AuthUser.js

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -8,7 +8,7 @@ export const verifyUser = async (req, res, next) => {
     where: {
       uuid: req.session.userId,
     },
-  });
+  }) || {};
   if (user.status) return res.status(406).json({ msg: "you are banned" });
   if (!user) return res.status(404).json({ msg: "user not found" });
   req.userId = user.id;
