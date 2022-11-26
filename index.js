3d1cca7cdd39569e3cdec2af79df159fcef34acc
Egor Kirdyapin <74021542+loqumi@users.noreply.github.com>
2022-11-26 20:00:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
 import db from "./config/Database.js";
-import SequelizeStore from "connect-session-sequelize";
 import UserRoute from "./routes/UserRoute.js";
 import AuthRoute from "./routes/AuthRoute.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
-
-const sessionStore = SequelizeStore(session.Store);
-
-const store = new sessionStore({
-  db: db,
-});
 
 (async () => {
   await db.sync();
@@ -26,7 +19,6 @@ app.use(
     secret: "kdjskllkmckzlncjk23u8921uijcxlcjkjc89sdjsadjlj389123892nuc8ah9ry3",
     resave: false,
     saveUninitialized: true,
-    store: store,
   })
 );
 
@@ -39,8 +31,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(UserRoute);
 app.use(AuthRoute);
-
-store.sync();
 
 app.listen(process.env.PORT, () => {
   console.log("Server up and running...");
