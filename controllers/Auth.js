d80ad1e826cd0ed6482249a4946142e55eba66ab
Egor Kirdyapin <74021542+loqumi@users.noreply.github.com>
2022-11-26 18:13:49 +0500

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -7,18 +7,18 @@ export const Login = async (req, res) => {
       email: req.body.email,
     },
   });
+  if (user.status) return res.status(406).json({ msg: "you are banned" });
   if (!user) return res.status(404).json({ msg: "user not found" });
   const match = await argon2.verify(user.password, req.body.password);
   if (!match) return res.status(400).json({ msg: "wrong password" });
   req.session.userId = user.uuid;
-  console.log(req.session, user, 1111111111111111111111111111)
+  res.setHeader("Set-Cookie", user.uuid)
   const uuid = user.uuid;
   const name = user.name;
   const email = user.email;
   res.status(200).json({ uuid, name, email });
 };
 export const Me = async (req, res) => {
-  console.log(req.session, 22222332321312321312321321321321312312312)
   if (!req.session.userId) {
     return res.status(401).json({ msg: "Please login to your account!" });
   }
