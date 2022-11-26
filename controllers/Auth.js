3b325f8ecc9715d8699973dc7ad94feef8cdc47c
Egor Kirdyapin <74021542+loqumi@users.noreply.github.com>
2022-11-26 20:30:39 +0500

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -15,7 +15,7 @@ export const Login = async (req, res) => {
   const name = user.name;
   const email = user.email;
   res
-    .cookie("token", user.uuid, { httpOnly: true, sameSite: "lax" })
+    .cookie("token", user.uuid, { httpOnly: true, SameSite="none", Secure })
     .status(200)
     .json({ uuid, name, email });
 };
